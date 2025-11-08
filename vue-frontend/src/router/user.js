4522261ff7247a2891b4a06b4d3e134396c43c9e
duyen <duyenltmpk04047@gmail.com>
2025-11-08 16:11:20 +0700

    fix loi

--- vue-frontend/src/router/user.js
+++ vue-frontend/src/router/user.js
@@ -1,28 +1,4 @@
 const users = [
-<<<<<<< HEAD
-    {
-        path: "/",
-        component: () => import("../layout/layoutUser.vue"),
-        children: [
-            {
-                path: "",
-                name: "home",
-                component: () => import("../page/user/home.vue")
-            },
-            {
-                path: "/about",
-                name: "about",
-                component: () => import("../page/user/about.vue")
-            },
-            {
-                path: "contact",
-                name: "contact",
-                component: () => import("../page/user/Contact.vue"),
-            },
-        ]
-    }
-]
-=======
   {
     path: "/",
     component: () => import("../layout/layoutUser.vue"),
@@ -38,6 +14,11 @@ const users = [
         component: () => import("../page/user/about.vue"),
       },
       {
+        path: "contact",
+        name: "contact",
+        component: () => import("../page/user/Contact.vue"),
+      },
+      {
         path: "/notFound",
         name: "notFound",
         component: () => import("../page/user/notFound.vue"),
@@ -50,6 +31,5 @@ const users = [
     ],
   },
 ];
->>>>>>> 27a34306ddc43c12992fcbe9c015f7494926cfdb
 
 export default users;
