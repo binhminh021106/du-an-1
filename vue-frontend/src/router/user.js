47b7efd7779030cce4041017dd4513d8c8d05e69
tantuanhi <ttuann47@gmail.com>
2025-11-08 15:35:59 +0700

    update_notfound

--- vue-frontend/src/router/user.js
+++ vue-frontend/src/router/user.js
@@ -1,20 +1,25 @@
 const users = [
-    {
-        path: "/",
-        component: () => import("../layout/layoutUser.vue"),
-        children: [
-            {
-                path: "",
-                name: "home",
-                component: () => import("../page/user/home.vue")
-            },
-            {
-                path: "/about",
-                name: "about",
-                component: () => import("../page/user/about.vue")
-            },
-        ]
-    }
-]
+  {
+    path: "/",
+    component: () => import("../layout/layoutUser.vue"),
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: () => import("../page/user/home.vue"),
+      },
+      {
+        path: "/about",
+        name: "about",
+        component: () => import("../page/user/about.vue"),
+      },
+      {
+        path: "/notFound",
+        name: "notFound",
+        component: () => import("../page/user/notFound.vue"),
+      },
+    ],
+  },
+];
 
 export default users;
